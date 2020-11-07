bded8edb1c8696055d4d4ce1f75786728cf74664
Alde Rojas <hello@alde.io>
2020-11-06 19:31:59 -0600

    Implement hold w and tap f macros

--- keymap.c
+++ keymap.c
@@ -1,7 +1,15 @@
 #include QMK_KEYBOARD_H
 
+#define TAP_F_INTERVAL 80
+
+static bool holding_w = false;
+static bool tapping_f = false;
+static uint16_t last_tapped_f = 0;
+
 enum custom_keycodes {
-  UP_URL = SAFE_RANGE
+  HOLD_W = SAFE_RANGE,
+  TAP_F,
+  RESET_ALL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,18 +23,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_UP,
         LCA(KC_Q),           LCA(KC_W),           LCA(KC_E),           KC_AUDIO_VOL_DOWN,
         LCA(KC_A),           LCA(KC_S),           LCA(KC_D),           LCA(KC_F),
-        LWIN(LSFT(KC_S)),    LCA(KC_X),           LCA(KC_C),           LCA(KC_V)
+        LWIN(LSFT(KC_S)),    HOLD_W,              TAP_F,               RESET_ALL
     )
 };
 
+
+static void tap_f(void) {
+    tap_code(KC_F);
+    last_tapped_f = timer_read();
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case UP_URL:
+        case HOLD_W:
             if (record->event.pressed) {
-                SEND_STRING("http://1upkeyboards.com");
+                holding_w = !holding_w;
+
+                if (holding_w) {
+                    register_code(KC_W);
+                } else {
+                    unregister_code(KC_W);
+                }
             }
             return false;
-            break;
+        case TAP_F:
+            if (record->event.pressed) {
+                tapping_f = !tapping_f;
+
+                if (tapping_f) {
+                    tap_f();
+                }
+            }
+            return false;
+        case RESET_ALL:
+            if (record->event.pressed) {
+                if (holding_w) {
+                    unregister_code(KC_W);
+                }
+
+                holding_w = false;
+                tapping_f = false;
+            }
+            return false;
     }
     return true;
+}
+
+void matrix_scan_user(void) {
+    if (tapping_f && timer_elapsed(last_tapped_f) > TAP_F_INTERVAL) {
+        tap_f();
+    }
 }
